50e76c45e99b40a1dabe763eb4ae5635aa194aee
管复生 <975081801@qq.com>
2016-01-11 11:13:17 +0800

    Initial Commit

---
+++ GFSWaterflowView(瀑布流)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GFSWaterflowView(瀑布流)
+//
+//  Created by 管复生 on 16/1/11.
+//  Copyright © 2016年 GFS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
